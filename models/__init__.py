cfa4dd540f043da62bcc096d7c0b6ce08a79ae3c
cynthia <cynthialairumbe@gmail.com>
2025-10-21 22:54:58 +0300

    seeding data

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,23 @@ from .payment import Payment
 from .project_application import ProjectApplication
 from .project import Project
 from .review import Review
-from .skill import Skill
+from .skill import Skill, FreelancerSkill
 from .time_log import TimeLog
 from .user import User, FreelancerProfile, ClientProfile
+
+__all__ = [
+    'Deliverable',
+    'Invoice',
+    'Message',
+    'Milestone',
+    'Payment',
+    'ProjectApplication',
+    'Project',
+    'Review',
+    'Skill',
+    'FreelancerSkill',
+    'TimeLog',
+    'User',
+    'FreelancerProfile',
+    'ClientProfile',
+]
