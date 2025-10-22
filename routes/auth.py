e8a9c9336acf9c86e317689558f60fcb5462da23
Joshua Karanja <jokaranja26@gmail.com>
2025-10-22 12:45:19 +0300

    importation  of middlewares,limiter,jwt and other dependenancies

---
+++ routes/auth.py
@@ -0,0 +1,16 @@
+from flask import app, request, jsonify
+from flask_restx import Namespace, Resource, fields
+from extensions import db
+from models import User  # Your user model
+from datetime import datetime, timedelta, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from config import SECRET_KEY, JWT_ACCESS_EXPIRES, JWT_REFRESH_EXPIRES
+from middlewares.auth_middleware import login_required, role_required
+from flask_limiter.util import get_remote_address
+from flask_limiter import Limiter
+from flask_cors import CORS
+
+# Setup CORS
+
+CORS(app)
